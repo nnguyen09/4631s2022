09c3ea10e2d0081b9ff1741934c864d66c46a168
Ngan Nguyen <nnguyen09@northshore.edu>
2022-02-08 21:29:41 -0500

    update main

--- CatEssentialApp/app/src/main/java/com/example/catshoppingstoreapplcation/MainActivity.java
+++ CatEssentialApp/app/src/main/java/com/example/catshoppingstoreapplcation/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         txtLoginOnClick();
     }
-    //ughgugugh
+    //ughgugughgdgdgd
 
      public void txtLoginOnClick(){
         txtLogin.setOnClickListener(new View.OnClickListener() {
