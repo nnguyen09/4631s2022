99a3ba9f51546b6d933dd42176dbe797fdc00e65
Ngan Nguyen <nnguyen09@northshore.edu>
2022-02-08 21:48:54 -0500

    update main

--- CatEssentialApp/app/src/main/java/com/example/catshoppingstoreapplcation/MainActivity.java
+++ CatEssentialApp/app/src/main/java/com/example/catshoppingstoreapplcation/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.catshoppingstoreapplcation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init txtLogin
+        txtLogin = findViewById(R.id.txtLogin);
+    }
+
+    public void txtLoginOnClick(){
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(MainActivity.this, Login.class);
+                startActivity(login);
+            }
+        });
     }
 }
-// LOL
-//LOLOLOLOL
-
-//lololol
